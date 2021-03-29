c5a462e1e079c86dc09b8b356b90b6e16fa99f7c
Lucas <lucas.odonnelll@gmail.com>
2021-03-29 11:00:24 -0700

    added some flash messages

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -11,6 +11,7 @@ class KittensController < ApplicationController
     @kitten = Kitten.new(kitten_params)
 
     if @kitten.save
+      flash[:notice] = "Kitten has been created!"
       redirect_to kitten_path(@kitten)
     else
       render :new 
@@ -28,6 +29,7 @@ class KittensController < ApplicationController
   def update
     @kitten = Kitten.find(params[:id])
     if @kitten.update(kitten_params)
+      flash[:notice] = "Kitten has been updated"
       redirect_to kitten_path(@kitten)
     else
       render :edit
@@ -37,6 +39,7 @@ class KittensController < ApplicationController
   def destroy
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
+    flash[:notice] = "Kitten has been destroyed :("
     redirect_to root_path
   end
 
